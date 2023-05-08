3f1444cc8589af9f8463ef1b9051fe82775b471f
ivanpudin <ivanpudin9@gmail.com>
2023-05-08 22:10:58 +0300

    Define variable types & display data on posts page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,11 @@ interface Post {
   comments: Comment[]
 }
 
+interface Comment {
+  comment_user: string
+  comment: string
+}
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -21,6 +26,22 @@ export default function Home() {
   return (
     <main>
       <header className='w-screen flex flex-row items-center justify-start pt-4 pb-4 pr-4 pl-8 text-white bg-black'><h1>PHP Blog</h1></header>
+      {posts.map((post, i) => {
+        return(
+          <div key={i} className='m-8'>
+            <p>Author: {post.post_user}</p>
+            <p>{post.post}</p>
+            {post.comments.map((comment, j) => {
+              return(
+                <div key={j} className='ml-16'>
+                  <p>{comment.comment_user}:</p>
+                  <p>--{comment.comment}</p>
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
     </main>
   )
 }
